43b402aca11898519d2cc751c3e4c2ef7802dcc0
kailanesarah <kailanesarahpro@gmail.com>
2025-10-13 15:04:17 -0300

    feat(api/authentication): Adiciona autenticação por credentials

---
+++ api/authentication/urls.py
@@ -0,0 +1,9 @@
+from authentication.views import (CustomLoginView, CustomLogoutView,
+                                  CustomRegisterView)
+from django.urls import path
+
+urlpatterns = [
+    path("auth/login/", CustomLoginView.as_view(), name="custom_login"),
+    path("auth/logout/", CustomLogoutView.as_view(), name="custom_logout"),
+    path("auth/registration/", CustomRegisterView.as_view(), name="custom_register"),
+]
