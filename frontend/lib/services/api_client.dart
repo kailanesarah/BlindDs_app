ef7b522fc7b1c564b8c2b9025947d816d5a19805
kailanesarah <kailanesarahpro@gmail.com>
2025-11-10 13:50:50 -0300

    refactor(lib/services): Refatora services. Agora todos utilizam o dio para conversar com a api

--- frontend/lib/services/api_client.dart
+++ frontend/lib/services/api_client.dart
@@ -1,44 +1,72 @@
+import 'package:blindds_app/config/app_config.dart';
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:blindds_app/services/refresh_service.dart';
+import 'dart:developer';
 
 class ApiClient {
-  final Dio dio = Dio(BaseOptions(baseUrl: 'http://10.0.2.2:8000/api/v1/'));
-  late final RefreshService refreshService;
+  final Dio dio = Dio(BaseOptions(baseUrl: AppConfig.baseURL));
+  late final RefreshService refreshService = RefreshService(dio);
 
   ApiClient() {
-    refreshService = RefreshService(dio); // injeta o RefreshService
-
-    dio.interceptors.add(
-      InterceptorsWrapper(
-        onRequest: (options, handler) async {
-          final prefs = await SharedPreferences.getInstance();
-          final token = prefs.getString('access');
-
-          if (token != null) {
-            options.headers['Authorization'] = 'Bearer $token';
-          }
-
-          return handler.next(options);
-        },
-
-        onError: (e, handler) async {
-          if (e.response?.statusCode == 401) {
-            final newToken = await refreshService.refreshToken();
-
-            if (newToken != null) {
+    try {
+      dio.interceptors.add(
+        InterceptorsWrapper(
+          //Intercepta a requisição antes de ser enviada
+          onRequest: (options, handler) async {
+            try {
               final prefs = await SharedPreferences.getInstance();
-              await prefs.setString('access', newToken);
+              final token = prefs.getString('access');
 
-              e.requestOptions.headers['Authorization'] = 'Bearer $newToken';
-
-              final cloneReq = await dio.fetch(e.requestOptions);
-              return handler.resolve(cloneReq);
+              if (token != null) {
+                options.headers['Authorization'] = 'Bearer $token';
+              }
+            } catch (e, stack) {
+              log(
+                'Erro ao recuperar token do SharedPreferences: $e',
+                stackTrace: stack,
+              );
             }
-          }
-          return handler.next(e);
-        },
-      ),
-    );
+
+            return handler.next(options);
+          },
+
+          // Intercepta erros (como 401 Unauthorized)
+          onError: (DioException e, handler) async {
+            try {
+              if (e.response?.statusCode == 401) {
+                log('Token expirado. Tentando atualizar...');
+
+                final newToken = await refreshService.refreshToken();
+
+                if (newToken != null) {
+                  final prefs = await SharedPreferences.getInstance();
+                  await prefs.setString('access', newToken);
+
+                  e.requestOptions.headers['Authorization'] =
+                      'Bearer $newToken';
+
+                  // Reenvia a requisição original com o novo token
+                  final cloneReq = await dio.fetch(e.requestOptions);
+
+                  return handler.resolve(cloneReq);
+                } else {
+                  log('Falha ao renovar o token: refresh retornou null');
+                }
+              }
+            } catch (err, stack) {
+              log(
+                'Erro ao tentar renovar token ou refazer requisição: $err',
+                stackTrace: stack,
+              );
+            }
+
+            return handler.next(e);
+          },
+        ),
+      );
+    } catch (e, stack) {
+      log('Erro ao configurar interceptors do Dio: $e', stackTrace: stack);
+    }
   }
 }
--- frontend/lib/services/login_google_service.dart
+++ frontend/lib/services/login_google_service.dart
@@ -4,31 +4,26 @@ import 'package:blindds_app/utils/helpers/generic_error_helper.dart';
 import 'package:dio/dio.dart';
 import 'package:blindds_app/services/api_client.dart';
 
-class LoginService {
+class LoginGoogleService {
   final ApiClient apiClient = ApiClient();
   late final Dio _dio;
 
-  LoginService() {
+  LoginGoogleService() {
     _dio = apiClient.dio;
   }
 
-  Future<Response> loginUser({
-    required String email,
-    required String password,
-  }) async {
+  Future<Response> loginWithGoogle({required String idToken}) async {
     try {
       final response = await _dio.post(
-        'auth/login/',
-        data: {'email': email, 'password': password},
+        'auth/social/',
+        data: {'firebase_id_token': idToken},
       );
+
       return response;
     } on DioException catch (e) {
       final message = DioErrorHelper.handle(e);
 
-      if (e.response?.statusCode == 400) {
-        // Erros de autenticação
-        throw AuthException(message);
-      } else if (e.type == DioExceptionType.connectionError) {
+      if (e.type == DioExceptionType.connectionError) {
         // Erros de rede
         throw NetworkException(message);
       } else {
