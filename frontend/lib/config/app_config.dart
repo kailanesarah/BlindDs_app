231183f94c829aa91408132b54b06c289bbe905d
kailanesarah <kailanesarahpro@gmail.com>
2025-11-10 13:46:11 -0300

    feat(lib/config): Adiciona arquivo de configuração e leitura de variáveis estáticas(como a url do back), por exemplo, trabalhar dessa forma deixa o código mais limpo e modularizado

---
+++ frontend/lib/config/app_config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+// Variáveis estáticas lidas APENAS UMA VEZ.
+class AppConfig {
+  static final String baseURL = dotenv.env['DJANGO_AUTH_URL'] ?? '';
+  AppConfig._();
+}
