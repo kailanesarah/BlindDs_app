0e8a1a13c54a3582a1ed83c1f1927e2e607c1da9
kailanesarah <kailanesarahpro@gmail.com>
2025-11-17 16:19:53 -0300

    fix: corrige comando seed

--- api/seed.py
+++ api/seed.py
@@ -1,23 +1,27 @@
 import os
 import django
-from homework.models import HomeworkModel
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cors.settings")
 django.setup()
 
+from homework.models import HomeworkModel
+
 
 def run():
-    atividade = HomeworkModel.objects.create(
+    atividade, created = HomeworkModel.objects.get_or_create(
         atv_name="Estrutura de dados fila",
-        atv_description="Revisar pilha, viewsets e autenticação JWT.",
+        defaults={"atv_description": "Revisar pilha, viewsets e autenticação JWT."},
     )
 
-    print("--------------- SEED GERADO ---------------")
-    print("Atividade criada:")
-    print(f"Nome: {atividade.atv_name}")
-    print(f"Descrição: {atividade.atv_description}")
-    print(f"Código gerado: {atividade.atv_code}")
-    print("-------------------------------------------")
+    if created:
+        print("--------------- SEED GERADO ---------------")
+        print("Atividade criada:")
+        print(f"Nome: {atividade.atv_name}")
+        print(f"Descrição: {atividade.atv_description}")
+        print(f"Código gerado: {atividade.atv_code}")
+        print("-------------------------------------------")
+    else:
+        print("Seed já existente, nada criado.")
 
 
 if __name__ == "__main__":
